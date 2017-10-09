fe3bf7294fa53aeacbcd02a2baf3f334011e2dd6
Tom Henderson <tom@mathpunk.net>
2017-10-09 09:51:02 -0500

    Create a mock request in the mock server. (Should be own class?)

--- src/main/java/HttpServer/MockSynchronousServer.java
+++ src/main/java/HttpServer/MockSynchronousServer.java
@@ -3,11 +3,17 @@ package HttpServer;
 public class MockSynchronousServer implements ISynchronousServer {
 
     private int port;
+    public String[] request;
+    public String[] response;
 
     @Override
     public void nextConnection() {
         port = 5000;
         System.out.println("If I were real, I'd be listening on port " + port);
+        String[] request = {"GET /index.html HTTP/1.1\r\n",
+                "Host: www.example.net\r\n",
+                "Accept-Language: en\r\n"};
+        String[] response = {};
     }
 
     @Override
@@ -17,6 +23,5 @@ public class MockSynchronousServer implements ISynchronousServer {
 
     @Override
     public void writeLine(String line) {
-
     }
 }
