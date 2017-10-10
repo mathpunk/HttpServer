c98b418726beba894757a1ba9e361f1b2e9d38aa
Tom Henderson <tom@mathpunk.net>
2017-10-10 12:18:12 -0500

    Friendly output messages

--- src/main/java/HttpServer/Service.java
+++ src/main/java/HttpServer/Service.java
@@ -3,36 +3,61 @@ package HttpServer;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Objects;
 import java.util.stream.Stream;
 
 public class Service {
     public static void main(String[] args) throws Exception {
-        int port = 5000;
+        int port = 1337;
+        String directory = null;
+
+        System.out.println("Processing command line arguments");
+        for (int i=0; i < args.length; i++) {
+            String token = args[i];
+            if (Objects.equals(token, "-p")) {
+                port = Integer.parseInt(args[i+1]);
+            } else if (token.equals("-d")) {
+                directory = args[i+1];
+            }
+        }
+
         System.out.println("Listening on " + port);
+        System.out.println("Serving resources at " + directory);
         ServerSocket listener = new ServerSocket(port);
 
         while(true) {
             System.out.println("Accepting connections");
             Socket io = listener.accept();
 
-            System.out.println("Connected");
+            System.out.println("\nConnected");
             BufferedReader reading = new BufferedReader(new InputStreamReader(io.getInputStream()));
             PrintWriter writing = new PrintWriter(io.getOutputStream(), true);
 
-            System.out.println("Interacting");
+            System.out.println("Request:");
             String line = reading.readLine();
             do {
                 System.out.println("I heard: " + line);
                 line = reading.readLine();
             } while (!line.isEmpty());
 
-            System.out.println("Responding");
+            System.out.println("\nResponding with:");
 
-            writing.println("HTTP/1.1 200 OK");
-            writing.println("Content-Length: 0");
+            String requestLine = "HTTP/1.1 200 OK";
+            System.out.println(requestLine);
+            writing.println(requestLine);
+
+            String contentType = "Content-Type: text/html";
+            System.out.println(contentType);
+            writing.println(contentType);
+
+            String contentLength = "Content-Length: 0";
+            System.out.println(contentLength);
+            writing.println(contentLength);
+
+            System.out.println("<CRLF>");
             writing.println("");
 
-            System.out.println("Closing connection");
+            System.out.println("\nClosing connection\n");
         }
     }
 }
