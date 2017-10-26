1f92177ed86f31bfeef97df7c1eb701751d5cec7
Tom Henderson <tom@mathpunk.net>
2017-10-26 13:21:22 -0500

    Define cob specific setters

--- src/main/java/HttpServer/Controller.java
+++ src/main/java/HttpServer/Controller.java
@@ -4,29 +4,39 @@ import java.util.HashMap;
 
 public class Controller {
 
-    public final Response ok;
-    public final Response notFound;
-    private final Response teapot;
     private final Router router;
-    public HashMap routes;
 
     public Controller() {
         Routes secretRoutes = new Routes();
         Router secretRouter = new Router(secretRoutes);
         this.router = secretRouter;
-        routes = new HashMap();
 
-        this.ok = new Response().setStatus(200);
-        this.notFound = new Response().setStatus(404);
-        this.teapot = new Response().setStatus(418).putBody("I'm a teapot");
-
-        defineRoute("GET", "/", ok);
-        defineRoute("PUT", "/form", ok);
-        defineRoute("GET", "/tea", ok);
+        Response ok = new Response().setStatus(200);
         defineRoute("HEAD", "/", ok);
-        defineRoute("GET", "/coffee", teapot);
+
+        serveRoot();
+        serveForm();
+        serveTea();
     }
 
+    public void serveRoot() {
+        Response ok = new Response().setStatus(200);
+        defineRoute("GET", "/", ok);
+    }
+
+    public void serveForm() {
+        Response ok = new Response().setStatus(200);
+        defineRoute("GET", "/form", ok);
+        defineRoute("PUT", "/form", ok);
+    }
+
+    public void serveTea() {
+        Response teaResponse = new Response().setStatus(200);
+        Response coffeeResponse = new Response().setStatus(418).putBody("I'm a teapot");
+
+        defineRoute("GET", "/tea", teaResponse);
+        defineRoute("GET", "/coffee", coffeeResponse);
+    }
 
     public void defineRoute(String verb, String uri, Response response) {
         RequestHandler handler = new RequestHandler((request) -> response);
