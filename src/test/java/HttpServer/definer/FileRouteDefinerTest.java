0890f3565f133e6921bba909f8d688af027ee2da
Thomas Henderson <tom@mathpunk.net>
2017-11-02 14:58:12 -0500

    Create FileResource class

--- src/test/java/HttpServer/definer/FileRouteDefinerTest.java
+++ src/test/java/HttpServer/definer/FileRouteDefinerTest.java
@@ -1,10 +1,12 @@
 package HttpServer.definer;
 
+import HttpServer.response.Response;
+import HttpServer.router.Router;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.IsCollectionContaining.hasItems;
@@ -32,11 +34,10 @@ public class FileRouteDefinerTest {
                 "file2", "image.jpeg", "partial_content.txt", "text-file.txt"));
     }
 
-    @Test
-    public void itCanAccessFileContent() throws IOException {
-        String filename = "text-file.txt";
-        String content = definer.getFileContent(filename);
-        String expectedContent = "file1 contents";
-        assertEquals(expectedContent, content);
+    @Ignore
+    public void itMakesContentGettable() {
+       Router router = definer.getRouter();
+       Response response = router.route("GET", "/text-file.txt");
+       assertEquals("file1 contents", response.getBody());
     }
 }
