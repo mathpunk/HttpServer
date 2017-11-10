94d30a4786d72cd6d590ed159d953bc1fb088aa0
Thomas Henderson <tom@mathpunk.net>
2017-11-09 22:31:46 -0800

    Handle IOExceptions in Responders with 500 status
    It seemed very clear where to create the response with a 500 status once
    the compiler explained how IOExceptions get thrown and must eventually be
    caught. Learning to raise exceptions myself is not on the critical path
    for this iteration, and so this is not tested. I will add that to the
    backlog.

--- src/main/java/HttpServer/core/responder/service/Service.java
+++ src/main/java/HttpServer/core/responder/service/Service.java
@@ -4,11 +4,13 @@ import HttpServer.core.message.request.Request;
 import HttpServer.core.responder.Responder;
 import HttpServer.core.message.response.Response;
 
+import java.io.IOException;
+
 public interface Service extends Responder {
 
     // ArrayList<Clause> clausesServed();
     // void register(Router router, Uri endpoint);
 
-    Response respond(Request request);
+    Response respond(Request request) throws IOException;
 }
 
